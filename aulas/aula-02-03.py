0b6b689b3e23ee3b8e7de5fe0fc6566b54816ce8
Davi-Fran <dafaosi.avlis@gmail.com>
2026-03-02 08:40:18 -0300

    Aula 02-03-26: Adicionando o exercício 3

--- aulas/aula-02-03.py
+++ aulas/aula-02-03.py
@@ -8,8 +8,17 @@
 # print(f"\nA média dos números {x}, {y} e {z} é igual a {average:.2f}")
 
 # EXERCÍCIO 2
-value = float(input("Digite o valor da prestação: "))
+# value = float(input("Digite o valor da prestação: "))
+#
+# fee = value * 1.1
+#
+# print(f"\nFoi aplicada à prestação de R$ {value:.2f} um juros de 10%, sendo assim, a nova prestação é R$ {fee:.2f}")
 
-fee = value * 1.1
+# EXERCÍCIO 3
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite um outro número: "))
+n3 = float(input("Digite um terceiro número: "))
 
-print(f"\nFoi aplicada à prestação de R$ {value:.2f} um juros de 10%, sendo assim, a nova prestação é R$ {fee:.2f}")
+average = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / 10
+
+print(f"\nA média ponderada dos valores {n1}, {n2} e {n3} é igual a {average:.1f}")
