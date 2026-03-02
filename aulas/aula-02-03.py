928de8b823e90297ad97da7268bfcf6a791aec1f
Davi-Fran <dafaosi.avlis@gmail.com>
2026-03-02 08:28:42 -0300

    Aula 02-03-26: Adicionando o exercício 2

--- aulas/aula-02-03.py
+++ aulas/aula-02-03.py
@@ -1,8 +1,15 @@
-# Exercício 1
-x = float(input("Digite um número: "))
-y = float(input("Digite um outro número: "))
-z = float(input("Digite um terceiro número: "))
+# EXERCÍCIO 1
+# x = float(input("Digite um número: "))
+# y = float(input("Digite um outro número: "))
+# z = float(input("Digite um terceiro número: "))
+#
+# average = (x + y + z) / 3
+#
+# print(f"\nA média dos números {x}, {y} e {z} é igual a {average:.2f}")
 
-average = (x + y + z) / 3
+# EXERCÍCIO 2
+value = float(input("Digite o valor da prestação: "))
 
-print(f"\nA média dos números {x}, {y} e {z} é igual a {average:.2f}")
+fee = value * 1.1
+
+print(f"\nFoi aplicada à prestação de R$ {value:.2f} um juros de 10%, sendo assim, a nova prestação é R$ {fee:.2f}")
