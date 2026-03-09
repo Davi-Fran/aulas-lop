bf0aec6174a7d00869aeded53a3ab70eddd7d357
Davi-Fran <dafaosi.avlis@gmail.com>
2026-03-09 10:24:13 -0300

    Aula 09/03/26 (1)

---
+++ aulas/aula-09-03.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Seu nome é {nome}, você tem {idade} anos e tem {altura} de altura.")
