e76c4e6596d4a9a031e53e3160676c805844dc21
Davi Fran <dafaosi.avlis@gmail.com>
2026-03-26 16:01:51 -0300

    Fazendo exercício 06 da lista de condicionais de PBE

--- PBE/lista-condicionais/ex06.py
+++ PBE/lista-condicionais/ex06.py
@@ -3,16 +3,29 @@ mes = int(input('Insira o mês: '))
 ano = int(input('Insira o ano: '))
 
 is_ano_bissexto = ano % 4 == 0 and ano % 100 != 0 or ano % 100 == 0 and ano % 400 == 0
+is_data_valida = True
 
 print()
 
-if is_ano_bissexto and mes == 2 and dia > 29:
-    print('A data inserida é inválida!')
-elif mes == 2 and dia > 28:
-    print('A data inserida é inválida!')
-elif mes > 12:
-    print('A data inserida é inválida!')
-elif ano < 1900 or ano > 2026:
-    print('Data inserida é inválida!')
+if ano > 1900 and ano < 2026:
+    if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+        if dia > 31:
+            is_data_valida = False
+    elif mes == 2:
+        if is_ano_bissexto:
+            if dia > 29:
+                is_data_valida = False
+        else:
+            if dia > 28:
+                is_data_valida = False
+    else:
+        if dia > 30:
+            is_data_valida = False
 else:
-    print(f'A data digitada foi: {dia:02d}:{mes:02d}:{ano:02d}')
+    is_data_valida = False
+    
+    
+if is_data_valida:
+    print(f'A data digitada {dia:02d}/{mes:02d}/{ano:02d}')
+else:
+    print('A data digitada é inválida!')
