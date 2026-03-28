39b1fee379d7a6a1d2b9b70c670c11da819f56b6
Davi-Fran <dafaosi.avlis@gmail.com>
2026-03-27 22:16:05 -0300

    Ajustando nomes & Tentando fazer o exercício 10

--- PBE/lista-condicionais/ex10.py
+++ PBE/lista-condicionais/ex10.py
@@ -1,10 +1,13 @@
 senha = input('Crie uma senha: ')
 
+tem_especial = not senha.isalnum()
+
 if len(senha) < 8:
     print('Senha inválida! Ela deve ter ao menos 8 caracteres!')
-elif senha.isalpha() and senha.islower() or senha.isnumeric():
-    print('Senha fraca')
-elif senha.isalnum() and senha.islower():
-    print('Senha média')
-elif '!@#$%&*()_-/\\' in senha:
-    print('Senha forte')
+else:
+    if tem_especial and senha.isupper() and senha.isalnum():
+        print('Senha forte')
+    elif senha.isalnum():
+        print('Senha média')
+    elif senha.isalpha() and senha.islower() or senha.isnumeric():
+        print('Senha fraca')
